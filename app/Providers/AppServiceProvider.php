69ef1046bf0c773274aae1d13e1a07dedb5ed539
Dan Harrin <git@danharrin.com>
2021-01-08 09:04:35 +0000

    Remove unused imports

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,13 +3,8 @@
 namespace App\Providers;
 
 use App\Models\App;
-use App\Models\LogEntry;
 use App\Models\User;
 use App\Models\Variable;
-use App\Observers\AppObserver;
-use App\Observers\LogEntryObserver;
-use App\Observers\UserObserver;
-use App\Observers\VariableObserver;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
