35f86ed59fd689687fe2ad3d22d9f8f523a2a90b
Dan Harrin <git@danharrin.com>
2021-01-08 09:28:30 +0000

    Fix formatting

--- resources/views/variables/create.blade.php
+++ resources/views/variables/create.blade.php
@@ -73,11 +73,7 @@
                             <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
                         </div>
 
-                        <div 
-                            @click.away="importOpen = false 
-                                        $wire.set('import', '')" 
-                            @keydown.escape.window="importOpen = false
-                                        $wire.set('import', '')"
+                        <div @click.away="importOpen = false" @keydown.escape.window="importOpen = false; $wire.set('import', '')"
                              class="relative z-10 bg-white rounded-lg overflow-hidden shadow-xl sm:max-w-4xl sm:w-full"
                         >
                             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
@@ -107,7 +103,6 @@
 
                                 <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
                                     <button @click="importOpen = false" type="button"
-                                            wire:click="$set('import', '')"
                                             wire:loading.class="opacity-75 cursor-wait"
                                             class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline transition ease-in-out duration-150 sm:text-sm sm:leading-5"
                                     >
